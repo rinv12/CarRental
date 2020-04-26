97f7f0349d557a4b2cc503404d2f3f5e4c1a8717
rinv12 <58483285+rinv12@users.noreply.github.com>
2020-04-25 22:13:00 -0600

    Create CarRental.php

---
+++ CarRental.php
@@ -0,0 +1,31 @@
+<html>
+<body>
+<?php
+echo "<h2>Car Rental Database</h2>";
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "carrentaldb";
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//check connection
+if($conn-> connect_error) {
+  die("Connection failed: ". $conn->connect_error);
+}
+echo "Connected Successfully <br>";
+echo "Showing the database results <br>";
+echo "------------------------------<br>;
+$sql = "Select ID_No, FN_Initial, Last_Name from customer";
+%result = $conn -> query($sql);
+if ($result)
+{
+  echo "Id No: .$row["ID_No"]. " ". $row["FN_Initial"]. " " .$row["Last_Name"]. "<br>";
+}else 
+{ echo "0 results";
+}
+
+?>
+</body>
+</html>

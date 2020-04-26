1533f5230ae88c0f0a0ca3991dd59ca467d463d4
rinv12 <58483285+rinv12@users.noreply.github.com>
2020-04-25 18:59:43 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+// a single line comment
+echo "Hello World!</br>";
+/*a multiple line comment print a variable*/
+$new_string = "Bye";
+echo $new_string;
+?>
+</body>
+</html>
